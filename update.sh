2d3f8f56a8e4f7f96284f885cb942126a0cc6567
manfrim <rafaelmanfrim2004@gmail.com>
2025-11-28 12:06:05 -0300

    Ajuste update.sh

--- update.sh
+++ update.sh
@@ -9,17 +9,16 @@ cd /var/www/sync-love-api
 echo "Executando git pull..."
 git pull
 
-echo "Instalando dependências do backend..."
-cd backend
+echo "Instalando dependências..."
 npm install
 
-echo "Realizando build do backend..."
+echo "Realizando build..."
 npm run build
 
 echo "Rodando migrations do banco de dados..."
 npx prisma migrate deploy
 
-echo "Reiniciando o backend no PM2..."
+echo "Reiniciando no PM2..."
 pm2 restart sync-love-api
 pm2 start ecosystem.config.js
 pm2 save
